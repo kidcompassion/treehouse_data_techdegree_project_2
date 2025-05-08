c132ffc063283b7aa8ae0b6c736e2203b2a4fe20
Sally Poulsen <sallypoulsen@Sallys-MacBook-Pro.local>
2025-05-08 12:34:59 -0600

    First commit

---
+++ app.py
@@ -0,0 +1,17 @@
+import constants
+
+CONSTANT_TEAMS = constants.TEAMS 
+CONSTANT_PLAYERS = constants.PLAYERS
+
+
+# clean data function
+
+
+# balance_teams function
+
+
+
+
+
+if __name__ == "__main__":
+    print(CONSTANT_PLAYERS)
